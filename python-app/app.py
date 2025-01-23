5d4ad2c65ef1466813465a31956cd2fb6c16b836
Souvik-Cyclic <Souvik-Cyclic@users.noreply.github.com>
2025-01-23 16:25:36 +0000

    chore(Python): Automated Code Formatting

--- python-app/app.py
+++ python-app/app.py
@@ -2,9 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-                                return 'Hello, World! Hi, I am a Python Flask application.'
 
-if __name__ == '__main__':
+@app.route("/")
+def hello_world():
+    return "Hello, World! Hi, I am a Python Flask application."
+
+
+if __name__ == "__main__":
     app.run(debug=True)
