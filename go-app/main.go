261dd32ab9fd6698e578408d2c7956f3001ae95d
Souvik-Cyclic <souvik.cyc@gmail.com>
2025-01-23 17:50:30 +0530

    fix: correct indentation in main.go for improved readability

--- go-app/main.go
+++ go-app/main.go
@@ -1,17 +1,17 @@
-package main
+									package main
 
-import (
-	"github.com/gofiber/fiber/v2"
-)
+									import (
+										"github.com/gofiber/fiber/v2"
+									)
 
-func main() {
-	app := fiber.New()
+									func main() {
+										app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.JSON(fiber.Map{
-			"message": "Hello, World!",
-		})
-	})
+										app.Get("/", func(c *fiber.Ctx) error {
+											return c.JSON(fiber.Map{
+												"message": "Hello, World!",
+											})
+										})
 
-	app.Listen(":8080")
-}
+										app.Listen(":8080")
+									}
