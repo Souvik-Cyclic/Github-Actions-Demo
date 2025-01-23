f36e4864ddbf881c6d2a0dafcc39f62b1ebd6035
Souvik-Cyclic <souvik.cyc@gmail.com>
2025-01-23 21:50:30 +0530

    fix: correct indentation in hello_world function in app.py

--- python-app/app.py
+++ python-app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World! Hi, I am a Python Flask application.'
+                        return 'Hello, World! Hi, I am a Python Flask application.'
 
 if __name__ == '__main__':
     app.run(debug=True)
