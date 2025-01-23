46c398a0fad4d4adfa5c3474bb67a65b9337cc9b
Souvik-Cyclic <Souvik-Cyclic@users.noreply.github.com>
2025-01-23 12:15:17 +0000

    [CHORE] Automated Code Formatting

--- go-app/main.go
+++ go-app/main.go
@@ -7,11 +7,11 @@ import (
 func main() {
 	app := fiber.New()
 
-					app.Get("/", func(c *fiber.Ctx) error {
-			return c.JSON(fiber.Map{
-											"message": "Hello, World!",
-								})
-				})
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"message": "Hello, World!",
+		})
+	})
 
 	app.Listen(":8080")
 }
