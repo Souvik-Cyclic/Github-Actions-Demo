0081da8229e240a33d0b08d17dc3a37ffaaf0a87
Souvik-Cyclic <souvik.cyc@gmail.com>
2025-01-23 21:49:02 +0530

    feat: update hello_world function to include additional greeting message

--- python-app/app.py
+++ python-app/app.py
@@ -4,8 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-                
-        return 'Hello, World!'
+    return 'Hello, World! Hi, I am a Python Flask application.'
 
 if __name__ == '__main__':
     app.run(debug=True)
